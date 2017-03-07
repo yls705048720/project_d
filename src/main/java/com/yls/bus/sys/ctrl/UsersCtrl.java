cedbdad18bc614d6afda01c1ec7ee61616cab8ec
Lianshan Yang <705048720@qq.com>
2017-03-07 22:26:38 +0800

    config

--- src/main/java/com/yls/bus/sys/ctrl/UsersCtrl.java
+++ src/main/java/com/yls/bus/sys/ctrl/UsersCtrl.java
@@ -3,10 +3,29 @@
  */
 package com.yls.bus.sys.ctrl;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.yls.bus.sys.dao.entity.Users;
+import com.yls.bus.sys.service.UsersService;
+
 /**
  * @author YLS
  *
  */
+@Controller
+@RequestMapping("/user")
 public class UsersCtrl {
-
+	
+	@Autowired 
+	private UsersService usersService;
+	
+	@RequestMapping("/register")
+	@ResponseBody
+	public String register( Users user){
+		usersService.addUser(user);
+		return user.toString();
+	}
 }
--- src/main/java/com/yls/bus/sys/service/UsersService.java
+++ src/main/java/com/yls/bus/sys/service/UsersService.java
@@ -3,10 +3,13 @@
  */
 package com.yls.bus.sys.service;
 
+import com.yls.bus.sys.dao.entity.Users;
+
 /**
  * @author YLS
  *
  */
 public interface UsersService {
 
+	public void addUser(Users users);
 }
